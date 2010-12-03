05369f31bcf3d6ce4c869582fd2946134ddb7f41
hi.kiyos@gmail.com <hi.kiyos@gmail.com@9e94ea02-8df7-a371-4551-cc9972368d4c>
2010-12-03 12:38:35 +0000

    заливаю оригинал webdkp r69

---
+++ site/themes/default/footer.tmpl.php
@@ -0,0 +1,6 @@
+<div id="footer">
+	Created by <a href="http://www.zeddic.com">Scott Bailey</a>
+	<br />
+	<?php /*Copyright &copy; OCAS, 2000 - 2007 <br />
+	<?=$processTime?> Seconds, <?=$queryCount?> Queries */ ?>
+</div>
