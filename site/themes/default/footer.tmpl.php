d280824484ad6ec4633b96faa8271abce23c9b57
arnisakalev@mail.ru <arnisakalev@mail.ru@9e94ea02-8df7-a371-4551-cc9972368d4c>
2010-12-14 11:32:01 +0000

    поправил created by

--- site/themes/default/footer.tmpl.php
+++ site/themes/default/footer.tmpl.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	Created by Scott Bailey. ruRU localization by Arnis aka Lev & hi.kiyos</a>
+	Created by <a href="http://www.zeddic.com/">Scott Bailey</a>. ruRU localization by <a href="http://code.google.com/p/webdkp-ru/">Arnis aka Lev & Kiyos</a>
 	<br />
 	<?php /*Copyright &copy; OCAS, 2000 - 2007 <br />
 	<?=$processTime?> Seconds, <?=$queryCount?> Queries */ ?>
